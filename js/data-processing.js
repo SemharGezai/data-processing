35858de21dcf86c294b8d7030036613a868ecf6b
Semhar Gezai <semhar.gezai206@gmail.com>
2022-01-25 05:37:26 -0800

    css/checkboxes/total

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,25 +1,54 @@
 
 //use location object to access querystring (address bar)
 const queryString = window.location.search;
-
 let myData = '';//will store data for output
-if(queryString!=""){//process data
+let myCart = '';//will store cart details
+let myTotal = 0;//will store total cost
+if(queryString != ""){//process data
+ 
+ 
   //separate querystring parameters
-const urlParams = new URLSearchParams(queryString);
+  const urlParams = new URLSearchParams(queryString);
 
 
-urlParams.forEach(function(value, key) {
+  urlParams.forEach(function(value, key) {
    
-    //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
-    // will replace underscore with spaces
-    key = key.split("_").join(" ");
-   
-    myData += `<p>${key}: ${value}</p>`;
-   //console.log(value, key);
- });
-myData += `<p><a href="index.html">CLEAR</a></p>`;
-document.getElementById("output").innerHTML = myData;
+    if(key=="Cart"){//process cart 
+      switch(value){
+        case "Widget":
+              myCart += "Widget: $3.99<br />";
+              myTotal += 3.99;
+        break;    
+
+        case "Sprocket":
+            myCart += "Sprocket: $5.99<br />";
+            myTotal += 5.99;
+        break;   
+
+        case "Thingy":
+            myCart += "Thingy: $1.99<br />";
+            myTotal += 1.99;
+        break;   
+      }    
+    }else{//build Shipping Info
+        //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
+        // will replace underscore with spaces
+        key = key.split("_").join(" ");
+        myData += `<p>${key}: ${value}</p>`;
+  
+    } 
+
+  });
+
+  myCart = `
+        <p><b>Cart Contents</b></p>
+        <p>${myCart}</p>
+        <p>Total: $${myTotal}</p>
+        `; 
+
+  myData = myCart + "<p><b>Shipping Information</b></p>" + myData;      
+  myData += `<p><a href="index.html">CLEAR</a></p>`;
+  
+  document.getElementById("output").innerHTML = myData;
+
 }    
-//output to console    
-//console.log(queryString);
-    
